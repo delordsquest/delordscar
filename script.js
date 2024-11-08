b57a26bd0cf2738ef5beb54b2e29520930e0e5b4
delords <delords@yahoo.com>
2024-11-08 16:14:02 +0100

    delordscar

---
+++ script.js
@@ -0,0 +1,5 @@
+setInterval(() => {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}, 5000);
